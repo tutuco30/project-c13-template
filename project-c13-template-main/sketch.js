0ce89dfb6f0fcd29b21e99cc8966d1b5b80b0deb
frs-marcelle <89610805+frs-marcelle@users.noreply.github.com>
2021-09-02 19:38:34 -0300

    Add files via upload

---
+++ project-c13-template-main/sketch.js
@@ -0,0 +1,36 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+ orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png"); 
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// mover o fundo
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//criar sprite do coelho
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+
+function draw() {
+  background(0);
+
+
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  drawSprites();
+  
+}
